a6bb31198a81ce0631cb2e2dec92f10ba1971e15
bjohne <97284793+bjohne@users.noreply.github.com>
2022-11-07 15:37:42 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <p>
-        open-sourced at {""}
-        <a
-          className="0pen-source-link"
-          href="https://github.com/bjohne/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>{" "}
-        by Betina Johne{" "}
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">hallo</button>
+        <p>
+          open-sourced at {""}
+          <a
+            className="0pen-source-link"
+            href="https://github.com/bjohne/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>{" "}
+          by Betina Johne{" "}
+        </p>
+      </div>
     </div>
   );
 }
